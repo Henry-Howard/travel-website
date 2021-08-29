5617ec0341a09ed2d517b9898abe1bc821c02843
Henry <eoducaysan@gmail.com>
2021-08-29 17:47:04 +0700

    upload web

---
+++ assets/js/main.js
@@ -0,0 +1,193 @@
+/*==================== SHOW MENU ====================*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+/*===== Hiện Menu =====*/
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+/*===== Ẩn Menu =====*/
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+/*==================== Xóa MENU MOBILE ====================*/
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  // Khi nhấp vào từng nav__link, sẽ xóa lớp show-menu
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/*==================== Thay Đổi Nền Tiêu Đề ====================*/
+function scrollHeader() {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 100) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
+
+/*==================== Slider Khám phá ====================*/
+let swiper = new Swiper(".discover__container", {
+  slidesPerView: "1",
+  spaceBetween: 0,
+  autoplay: false,
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+});
+
+/*==================== Video ====================*/
+const videoFile = document.getElementById("video-file"),
+  videoButton = document.getElementById("video-button"),
+  videoIcon = document.getElementById("video-icon");
+
+function playPause() {
+  if (videoFile.paused) {
+    // Phát video
+    videoFile.play();
+    // Thay đổi biểu tượng
+    videoIcon.classList.add("ri-pause-line");
+    videoIcon.classList.remove("ri-play-line");
+  } else {
+    // Dừng video
+    videoFile.pause();
+    // Thay đổi biểu tượng
+    videoIcon.classList.remove("ri-pause-line");
+    videoIcon.classList.add("ri-play-line");
+  }
+}
+videoButton.addEventListener("click", playPause);
+
+function finalVideo() {
+  // khi video kết thúc, thay đổi icon
+  videoIcon.classList.remove("ri-pause-line");
+  videoIcon.classList.add("ri-play-line");
+}
+// Dừng lại, khi video kết thúc (không phát lại nữa)
+videoFile.addEventListener("ended", finalVideo);
+
+/*==================== Hiện SCROLL UP ====================*/
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // Khi scroll cao hơn 200, thêm show-scroll vào scroll-top
+  if (this.scrollY >= 200) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
+
+/*==================== Liên kết các SCROLL hoạt động ====================*/
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
+
+/*==================== SCROLL REVEAL ANIMATION ====================*/
+const sr = ScrollReveal({
+  distance: "60px",
+  duration: 2800,
+  // reset: true,
+});
+
+sr.reveal(
+  `.home__data, .home__social-link, .home__info,
+           .discover__container,
+           .experience__data, .experience__overlay,
+           .place__card,
+           .sponsor__content,
+           .footer__data, .footer__rights`,
+  {
+    origin: "top",
+    interval: 100,
+  }
+);
+
+sr.reveal(
+  `.about__data, 
+           .video__description,
+           .subscribe__description`,
+  {
+    origin: "left",
+  }
+);
+
+sr.reveal(
+  `.about__img-overlay, 
+           .video__content,
+           .subscribe__form`,
+  {
+    origin: "right",
+    interval: 100,
+  }
+);
+
+/*==================== Theme DARK-lIGHT ====================*/
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "ri-sun-line";
+
+// Theme đã chọn trước (nếu người dùng đã chọn)
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+// Xác thực theme, chuyển sang dark-theme
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line";
+
+// xác nhận nếu trước đó đã chọn theme
+if (selectedTheme) {
+  // Nếu quá trình xác thực được hoàn thành, chuyển tới đã kích hoạt hay hủy kích hoạt dark-theme
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](
+    iconTheme
+  );
+}
+
+// Kích hoạt / hủy kích hoạt theme theo cách thủ công bằng click icon
+themeButton.addEventListener("click", () => {
+  // Thêm hoặc xóa dark theme / icon
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  // Save theme và icon hiện tại đã chọn
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
